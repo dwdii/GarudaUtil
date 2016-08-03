5c5460da91513aae1f8a506597b270bcea95c89e
Daniel Dittenhafer <daniel@dittenhafer.net>
2016-08-03 13:38:04 -0400

    BulkCopy skeleton

---
+++ Garuda.Data/PhoenixBulkCopy.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garuda.Data
+{
+    public class PhoenixBulkCopy
+    {
+        private PhoenixConnection _connection = null;
+
+        public PhoenixBulkCopy(PhoenixConnection connection)
+        {
+            if(null == connection)
+            {
+                throw new ArgumentNullException(nameof(connection));
+            }
+
+            this._connection = connection;
+        }
+
+        public int BatchSize { get; set; }
+
+        public void WriteToServer(DataTable table)
+        {
+                
+        }
+    }
+}
